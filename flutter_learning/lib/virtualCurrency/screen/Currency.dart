10e95fbdf800ca8b0b97786800852c409ff84c89
itssonu <sonu98gupta@gmail.com>
2021-02-13 22:34:26 +0530

    currency exchanger complete

--- flutter_learning/lib/virtualCurrency/screen/Currency.dart
+++ flutter_learning/lib/virtualCurrency/screen/Currency.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_learning/virtualCurrency/utilities/Network.dart';
 import 'package:flutter_learning/virtualCurrency/utilities/constants.dart';
 import 'dart:io' show Platform;
 
@@ -9,7 +10,42 @@ class Currency extends StatefulWidget {
 }
 
 class _CurrencyState extends State<Currency> {
-  String selectedCurrencyValue = 'INR';
+  String selectedCurrencySign = 'INR';
+  // ignore: non_constant_identifier_names
+  double BTCexchangeValue;
+  // ignore: non_constant_identifier_names
+  double ETHexchangeValue;
+  // ignore: non_constant_identifier_names
+  double LTCexchangeValue;
+  // String currencySign;
+
+  void updateUi() async {
+    BTCexchangeValue = 0.0;
+    ETHexchangeValue = 0.0;
+    LTCexchangeValue = 0.0;
+    Network btcNetwork = Network('BTC/$selectedCurrencySign');
+    Network ethNetwork = Network('ETH/$selectedCurrencySign');
+    Network ltcNetwork = Network('LTC/$selectedCurrencySign');
+    var data1 = await btcNetwork.getData();
+    var data2 = await ethNetwork.getData();
+    var data3 = await ltcNetwork.getData();
+    print(data1);
+    print(data2);
+    print(data3);
+    setState(() {
+      BTCexchangeValue = data1['rate'].roundToDouble();
+      ETHexchangeValue = data2['rate'].roundToDouble();
+      LTCexchangeValue = data3['rate'].roundToDouble();
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    updateUi();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,25 +59,21 @@ class _CurrencyState extends State<Currency> {
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Card(
-            margin: EdgeInsets.all(30.0),
-            color: Colors.blue[300],
-            elevation: 10.0,
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0)),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(60.0, 20.0, 60.0, 20.0),
-              child: Center(
-                  child: Text(
-                '1 BTC = ? USD',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                    letterSpacing: 1.5,
-                    fontWeight: FontWeight.bold),
-              )),
-            ),
-          )
+          CurrencyCard(
+            cryptoType: 'BTC',
+            exchangeValue: BTCexchangeValue,
+            selectedCurrencySign: selectedCurrencySign,
+          ),
+          CurrencyCard(
+            cryptoType: 'ETH',
+            exchangeValue: ETHexchangeValue,
+            selectedCurrencySign: selectedCurrencySign,
+          ),
+          CurrencyCard(
+            cryptoType: 'LTC',
+            exchangeValue: LTCexchangeValue,
+            selectedCurrencySign: selectedCurrencySign,
+          ),
         ],
       ),
       bottomNavigationBar: Container(
@@ -83,12 +115,14 @@ class _CurrencyState extends State<Currency> {
 
     return DropdownButton<String>(
       elevation: 15,
-      value: selectedCurrencyValue,
+      value: selectedCurrencySign,
       onChanged: (value) {
         print(value);
+
         setState(() {
-          selectedCurrencyValue = value;
+          selectedCurrencySign = value;
         });
+        updateUi();
       },
       items: getdropdown(),
     );
@@ -99,7 +133,7 @@ class _CurrencyState extends State<Currency> {
       var a = kcurrencyList.map<Text>((String value) {
         return Text(value);
       }).toList();
-      ;
+
       return a;
     }
 
@@ -112,6 +146,41 @@ class _CurrencyState extends State<Currency> {
           print(value);
         },
         children: dropdownItem(),
+      ),
+    );
+  }
+}
+
+class CurrencyCard extends StatelessWidget {
+  const CurrencyCard({
+    Key key,
+    @required this.exchangeValue,
+    @required this.selectedCurrencySign,
+    @required this.cryptoType,
+  });
+
+  final double exchangeValue;
+  final String selectedCurrencySign;
+  final String cryptoType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+      color: Colors.blue[300],
+      elevation: 10.0,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 20.0),
+        child: Center(
+            child: Text(
+          '1 $cryptoType = $exchangeValue $selectedCurrencySign',
+          style: TextStyle(
+              fontSize: 16.0,
+              color: Colors.white,
+              letterSpacing: 1.5,
+              fontWeight: FontWeight.bold),
+        )),
       ),
     );
   }
